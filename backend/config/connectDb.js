a2f20bf0dc0a5e693fb98919e8e277027d6e8cf3
Sanskar Saurabh <sanskarsaurabh16@gmail.com>
2026-02-09 14:35:24 +0530

    Fixed merge conflicts

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_DB, {
-      family: 4,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_DB);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
