630f719bd101022b0272ef14b1ee592ab83cb92f
SanskarSaurabh <sanskarsaurabh16@gmail.com>
2026-02-09 18:01:07 +0530

    sixth

--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.API_URL ,
+  baseURL: process.env.REACT_APP_API_URL ,
 });
 //|| "http://localhost:5000/api"  we use only for development
 API.interceptors.request.use((req) => {
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // This replaces the hardcoded localhost with your Render environment variable
-const socket = io(process.env.API_URL , {
+const socket = io(process.env.REACT_APP_API_URL , {
     transports: ["websocket", "polling"],
   });
 // || "http://localhost:5000/api"
