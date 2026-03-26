d25cf021a57ded81b1f3e8167f804d40f453cb6c
SanskarSaurabh <sanskarsaurabh16@gmail.com>
2026-03-26 23:11:04 +0530

    reset fix

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -135,7 +135,7 @@ export const forgotPassword = async (req, res) => {
   }
 });
 
-    const resetLink = `http://localhost:5173/reset-password/${token}`;
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password/${token}`;
 
     await transporter.sendMail({
       to: user.email,
